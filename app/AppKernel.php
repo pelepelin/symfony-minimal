ea0d0efba258d008dd8ea6ef544acca0c1597eec
Konstantin Pelepelin <konstantin.pelepelin@gmail.com>
2014-03-28 19:27:31 -0400

    Removed FrameworkExtraBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -12,7 +12,6 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SecurityBundle\SecurityBundle(),
             new Symfony\Bundle\TwigBundle\TwigBundle(),
             new Symfony\Bundle\MonologBundle\MonologBundle(),
-            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
--- src/Acme/DemoBundle/Controller/DemoController.php
+++ src/Acme/DemoBundle/Controller/DemoController.php
@@ -4,16 +4,10 @@ namespace Acme\DemoBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-// import the "@Template" annotation
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-
 class DemoController extends Controller
 {
-    /**
-     * @Template()
-     */
     public function indexAction()
     {
-        return array();
+        return $this->render('AcmeDemoBundle:Demo:index.html.twig');
     }
 }
